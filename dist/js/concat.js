58c366d1bca5eb097332f691b0c562ac12a21a33
msc <956164483@qq.com>
2017-03-14 14:36:06 +0800

    1231313

--- dist/js/concat.js
+++ dist/js/concat.js
@@ -15,7 +15,20 @@ $(document).ready(function(){
 		$('.service .right .right-item').addClass('hide').eq(index).removeClass('hide');
 	})
 })
-
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
 /**
  * Swiper 3.4.1
  * Most modern mobile touch slider and framework with hardware accelerated transitions
